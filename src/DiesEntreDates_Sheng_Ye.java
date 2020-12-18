5e21a4fcaadc250074c220304d7919c5b8d1b04d
sheng <shengyefe@gmail.com>
2020-12-18 13:32:45 +0100

    final

--- src/DiesEntreDates_Sheng_Ye.java
+++ src/DiesEntreDates_Sheng_Ye.java
@@ -8,42 +8,68 @@
  **/
 public class DiesEntreDates_Sheng_Ye extends CalcularDiesEntreDates {
 
-    @Override
-    protected int diesMes(int mes)
-    { return 0; }
+    //Array que contiene dia de meses.
+    private final int[] diaMes = new int[]{
+            31, 28, 31,
+            30, 31, 30,
+            31, 31, 30,
+            31, 30, 31
+    };
 
-    @Override
+    //Dia que tocaria en los meses
+    protected int diesMes(int mes) {
+        return this.diaMes[mes - 1];
+    }
+
+    //Se resta los dias de los meses con el numero de dia que le pasemos luego.
     protected int diesMesInicial(DataXS dataXS) {
-        return 0;
+        return  this.diesMes(dataXS.mes) - dataXS.dia;
     }
 
-    @Override
+    //Calcula restando los dias que han pasado desde el destino.
     protected int diesMesDesti(DataXS dataXS) {
-        return 0;
+        return dataXS.dia;
     }
 
-    @Override
+    //Calcula el resto de dias que quedan del año
     protected int diesResteAnyInicial(DataXS datainicial) {
-        return 0;
+        int diarestantsinicial = this.diesMesInicial(datainicial);
+        for (int i = datainicial.mes; i < 12; i++) {
+            diarestantsinicial += this.diaMes[i];
+        }
+        return diarestantsinicial - this.diesMesInicial(datainicial);
     }
 
-    @Override
+    //Calcular los dias que han pasado del año
     protected int diesResteAnyDesti(DataXS datadesti) {
-        return 0;
+        int diesrestantsdestino = this.diesMesDesti(datadesti);
+        for (int i = 0; i < datadesti.mes - 1; i++) {
+            diesrestantsdestino += diaMes[i];
+        }
+        return diesrestantsdestino - this.diesMesDesti(datadesti);
     }
 
-    @Override
+    //Calcular los dias entre dos fechas en que no sean bisiestos
     protected int diesNumAnysComplets(DataXS datainicial, DataXS datadesti) {
-        return 0;
+        int diasAnyComplets = 0;
+        for (int i = datainicial.any + 1; i < datainicial.any + (datadesti.any - datainicial.any); i++){
+            if (anyDeTraspas(i)) diasAnyComplets += 365;
+        }
+        return diasAnyComplets;
     }
 
-    @Override
+    //Calcular los dias entre dos fechas si son bisiestos
     protected int numDiesPerAnysdeTraspas(DataXS datainicial, DataXS datadesti) {
-        return 0;
+        int diasAnyTraspass = 0;
+        for (int i = datainicial.any + 1; i < datainicial.any + (datadesti.any - datainicial.any); i++){
+            if (anyDeTraspas(i)) diasAnyTraspass += 366;
+        }
+        return diasAnyTraspass;
     }
 
-    @Override
+    //pasa a calcular el año si es bisiesto
     protected boolean anyDeTraspas(int any) {
-        return false;
+        this.diaMes[1] = 29;
+        return (any % 400 == 0) || ((any % 4 == 0) && !(any % 100 == 0));
     }
 }
--- src/TestDiesEntreDates_Sheng_Ye.java
+++ src/TestDiesEntreDates_Sheng_Ye.java
@@ -7,19 +7,15 @@
  * Description:
  **/
 public class TestDiesEntreDates_Sheng_Ye {
-        private static DiesEntreDates_Sheng_Ye calcul;
-        private static DataXS inici, desti;
-
         public static void main(String[] args) {
-            calcul = new DiesEntreDates_Sheng_Ye();
+            DiesEntreDates_Sheng_Ye c1 = new DiesEntreDates_Sheng_Ye();
             try {
-                inici = new DataXS("12/12/2017");
-                desti = new DataXS("1/1/2018");
-                System.out.println(calcul.nombreDiesTotals(inici,desti));
-            } catch (Exception e){
+                DataXS Fechaprincipal = new DataXS("12/12/2017");
+                DataXS FechaFinal = new DataXS("01/01/2018");
+                System.out.println("Los dias entre dos fechas han sido: " +c1.nombreDiesTotals(Fechaprincipal,FechaFinal));
+            }catch (Exception e){
                 System.out.println(e.getMessage());
             }
         }
-
 
 }
